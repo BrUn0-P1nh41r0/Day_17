f1705d5d509df2c81f6871f6c940f887a7549924
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2024-12-14 16:50:02 +0000

    part 2 of quiz project complete

---
+++ main.py
@@ -0,0 +1,12 @@
+import data
+from question_model import Question
+
+question_bank = []
+
+for question in data.question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank[0].answer)
