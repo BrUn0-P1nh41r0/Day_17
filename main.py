31c11ce46c9b92fb593e6315ec3c7470a1b0d25a
Bruno <141152582+BrUn0-P1nh41r0@users.noreply.github.com>
2024-12-14 16:59:51 +0000

    quiz part 3 complete

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
-
 for question in data.question_data:
     question_text = question["text"]
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
